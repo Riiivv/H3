db811c407771acbc92e60bd7640fb51d4b875147
Mathias <Altenmat@msn.com>
2025-11-19 11:52:25 +0100

    Routes

--- FrontEnd/src/app/app.ts
+++ FrontEnd/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from "./home/home";
+import { Navbar } from './navbar/navbar';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Home, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
